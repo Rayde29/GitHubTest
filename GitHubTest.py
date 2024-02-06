1af75ad049a6cf9232c59067ba95c0e026552525
Rayde29 <raydek21@gmail.com>
2024-02-06 21:41:30 +0200

    Error, trying revert functionality.

--- GitHubTest.py
+++ GitHubTest.py
@@ -3,6 +3,6 @@ def Main () :
     print("Hello World")
     print("This has been changed")
     print("This seems wot work well")
-    print("Here's a change for Dad! :)")
+    print("Here's a change for Dad! :)"
 if __name__ == '__main__':
     Main()
